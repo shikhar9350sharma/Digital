75746826b89684bf7c0ef192d74c44bfb7a25876
shikhar9350sharma <shikharsharma593@gmail.com>
2026-02-03 00:34:40 +0530

    updated json file

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  eslint: {
-    // This is the important one for you!
-    // It tells Vercel: "Deploy even if I have small warnings like unused variables."
-    ignoreDuringBuilds: true,
-  },
+  /* config options here */
 };
 
 export default nextConfig;
