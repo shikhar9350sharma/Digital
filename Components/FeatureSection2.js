2d88d399eb3a2bdcca408e5eeec84e7488d3b4b6
shikhar9350sharma <shikharsharma593@gmail.com>
2026-02-02 20:11:59 +0530

    section 2 completed

--- Components/FeatureSection2.js
+++ Components/FeatureSection2.js
@@ -2,11 +2,18 @@ import React from 'react'
 
 const FeatureSection2 = () => {
   return (
-    <section className='border border-black w-93.75 h-192 flex flex-col gap-15 pt-15 px-5 pb-15 md:w-full md:h-170.5 md:gap-10 md:pt-20 md:pr-20 md:pb-30 md:pl-170  '>
-        <div className='border border-black w-83.75 h-44  md:h-33.5 md:gap-6 flex'>
+    <section className=' w-93.75 h-192 flex flex-col gap-15 pt-15 px-5 pb-15 md:w-full md:h-170.5 md:gap-10 md:pt-20 md:pr-20 md:pb-30 md:pl-170  '>
+        <div className=' w-83.75 h-44 gap-4.5 md:w-full md:h-33.5 md:gap-6 flex flex-col'>
             <h3 className='text-[#12161D] font-inter font-medium text-[20px] leading-7 md:text-[24px] md:leading-8 tracking-[0%]'>Strategic Location</h3>
             <p className='text-[#61656E] font-inter font-normal text-[18px] leading-6.5 md:text-[18px] md:leading-6.5 tracking-[0%] '>Cluster housing perumnas is located in an area that is easily accessible from various directions. You can reach the city center, airport, train station, bus terminal, and other important places easily and quickly.</p>
-
+        </div>
+        <div className=' w-83.75 h-44 gap-4.5 md:w-full md:h-33.5 md:gap-6 flex flex-col'>
+            <h3 className='text-[#12161D] font-inter font-medium text-[20px] leading-7 md:text-[24px] md:leading-8 tracking-[0%]'>Modern Design</h3>
+            <p className='text-[#61656E] font-inter font-normal text-[18px] leading-6.5 md:text-[18px] tracking-[0%] '>Cluster housing perumnas has a modern and elegant house design. You can choose the type of house that suits your taste and needs, ranging from type 36 to type 120.</p>
+        </div>
+        <div className=' w-83.75 h-50.5 gap-4.5 md:w-full md:h-40 md:gap-6 flex flex-col'>
+            <h3 className='text-[#12161D] font-inter font-medium text-[20px] leading-7 md:text-[24px] md:leading-8 tracking-[0%]'>Strategic Location</h3>
+            <p className='text-[#61656E] font-inter font-normal text-[18px] leading-6.5 md:text-[18px] md:leading-6.5 tracking-[0%] '>Cluster housing perumnas is located in an area that is easily accessible from various directions. You can reach the city center, airport, train station, bus terminal, and other important places easily and quickly.</p>
         </div>
 
     </section>
